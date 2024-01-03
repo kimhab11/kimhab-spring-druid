27e35d51d790ace778e9208b3c26c2af02cbf50c
kimhab11 <keomhab@gmail.com>
2024-01-03 11:18:02 +0700

    druid test

---
+++ src/main/java/com/example/kimhabspringdruid/KimhabSpringDruidApplication.java
@@ -0,0 +1,13 @@
+package com.example.kimhabspringdruid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimhabSpringDruidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimhabSpringDruidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kimhabspringdruid/KimhabSpringDruidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kimhabspringdruid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimhabSpringDruidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
